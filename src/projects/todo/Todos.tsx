9127dd6d1bd673c5c86b080800ed34106a3865c7
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-29 16:32:14 +0530

    persist tasks to local storage

--- src/projects/todo/Todos.tsx
+++ src/projects/todo/Todos.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./AddTask";
 
-const todosArray = [
+type TodosType = {
+  id: number;
+  task: string;
+  completed: boolean;
+};
+
+const initialTodosArray = [
   { id: 1, task: "Task One", completed: false },
   { id: 2, task: "Task Two", completed: true },
   { id: 3, task: "Task Three", completed: false },
 ];
 
 function Todos() {
-  const [tasksArray, setTasksArray] = useState(todosArray);
+  const [tasksArray, setTasksArray] = useState<TodosType[]>(() => {
+    const savedTodos = localStorage.getItem("localStorageArray");
+    return savedTodos ? JSON.parse(savedTodos) : initialTodosArray;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("localStorageArray", JSON.stringify(tasksArray));
+  }, [tasksArray]);
 
   const addTask = (taskTitle: string) => {
     const newId = Date.now();
@@ -17,7 +30,10 @@ function Todos() {
       task: taskTitle,
       completed: false,
     };
-    setTasksArray((prev) => [newTask, ...prev]);
+    setTasksArray((prev) => {
+      if (!prev) return [newTask];
+      return [newTask, ...prev];
+    });
   };
 
   const toggleCompleted = (id: number) => {
@@ -32,7 +48,7 @@ function Todos() {
       <AddTask onSubmit={addTask} />
       <div className="max-w-lg w-[90%] mx-auto pt-10">
         <ul>
-          {tasksArray.map(({ id, task, completed }) => {
+          {tasksArray?.map(({ id, task, completed }) => {
             return (
               <li key={id} className={`${completed && "line-through"}`}>
                 <input
